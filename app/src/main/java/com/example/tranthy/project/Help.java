54066f60ee808989a04e3ff899350df9acda2d1e
thytran142 <thytran142@gmail.com>
2014-10-26 17:28:42 +0800

    Finish Help!

---
+++ app/src/main/java/com/example/tranthy/project/Help.java
@@ -0,0 +1,46 @@
+package com.example.tranthy.project;
+
+import android.app.DialogFragment;
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.view.WindowManager;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.TextView;
+
+import java.sql.SQLException;
+
+/**
+ * Created by tranthy on 26/10/14.
+ */
+public class Help extends DialogFragment {
+    static String dialogTitle;
+    Button btn;
+    public Help(){}
+    public interface HelpInterface{
+
+    }
+    public void setDialogTitle(String title){
+        dialogTitle=title;
+    }
+    @Override
+    public View onCreateView(LayoutInflater inflater, ViewGroup container,Bundle savedInstanceState){
+        View view= inflater.inflate(R.layout.help,container);
+
+        btn=(Button)view.findViewById(R.id.btn_help_OK);
+        //Event handler for the button
+        btn.setOnClickListener(new View.OnClickListener(){
+            public void onClick(View view) {
+
+                    //dismiss the alert
+                    dismiss();}
+
+        });//end event handler for the button
+
+        //set the title for the dialog
+        getDialog().setTitle(dialogTitle);
+      return view;
+ }
+}
